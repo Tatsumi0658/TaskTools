11bc15fe2a9bb792eb8271d4832fe82dfe0ed264
koda <hogehogehoge@gmail.com>
2019-02-17 22:42:20 +0900

    user登録機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :set_user, only:[:edit, :update, :destroy, :show]
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to todotasks_path
+      flash[:success] = "ユーザーを作成しました"
+    else
+      render :new
+      flash[:danger] = "失敗しました"
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @user.update(set_user)
+      redirect_to todotasks_path
+      flash[:success] = "更新しました"
+    else
+      render :edit
+      flash[:danger] = "失敗しました"
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to todotasks_path
+    flash[:success] = "削除しました"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
