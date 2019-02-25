1639ab08cdb1a159b0e4c1c3afcde2b90ff97bb5
koda <hogehogehoge@gmail.com>
2019-02-26 00:42:29 +0900

    scope実装

--- app/controllers/todotasks_controller.rb
+++ app/controllers/todotasks_controller.rb
@@ -1,12 +1,14 @@
 class TodotasksController < ApplicationController
   before_action :set_task, only:[:edit, :destroy, :show, :update]
   def index
-    if params[:todotask].present? && params[:todotask][:name] && params[:todotask][:status].blank?
-      @todotasks = Todotask.where("name LIKE ?", "#{ params[:todotask][:name] }")
-    elsif params[:todotask].present? && params[:todotask][:name].blank? && params[:todotask][:status]
-      @todotasks = Todotask.where("status = ?", params[:todotask][:status] )
-    elsif params[:todotask].present? && params[:todotask][:name] && params[:todotask][:status]
-      @todotasks = Todotask.where("name LIKE ?", "#{ params[:todotask][:name] }").where("status = ?", params[:todotask][:status] )
+    if params[:todotask].present? && params[:todotask][:search_flag]
+      if params[:todotask][:name] && params[:todotask][:status].blank?
+        @todotasks = Todotask.search_name(params[:todotask][:name])
+      elsif params[:todotask][:name].blank? && params[:todotask][:status]
+        @todotasks = Todotask.search_status(params[:todotask][:status])
+      elsif params[:todotask][:name] && params[:todotask][:status]
+        @todotasks = Todotask.search_name(params[:todotask][:name]).search_status(params[:todotask][:status])
+      end
     elsif params[:sort_expired].present?
       @todotasks = Todotask.all.order('deadline desc')
     else
--- app/models/todotask.rb
+++ app/models/todotask.rb
@@ -1,4 +1,6 @@
 class Todotask < ApplicationRecord
   validates :name, presence: true
   belongs_to :user, optional: true
+  scope :search_name, -> (name){ where("name LIKE ?", name) }
+  scope :search_status, -> (status){ where(status: status) }
 end
