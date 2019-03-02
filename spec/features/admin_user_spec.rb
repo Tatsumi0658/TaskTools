2f7c0aebe07d55f2a43a5c3ffbbf984cc2d14303
koda <hogehogehoge@gmail.com>
2019-03-03 03:00:52 +0900

    test実装

---
+++ spec/features/admin_user_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.feature "adminuser機能", type: :feature do
+  scenario "登録" do
+    visit admin_users_path
+    find(".btn-success").click
+    fill_in "名前", with: "sample"
+    fill_in "メールアドレス", with:"sample@sample.com"
+    fill_in "パスワード", with: "password"
+    fill_in "もう一度入力してください", with:"password"
+    click_on "登録する"
+
+    expect(page).to have_content "ユーザーリスト"
+  end
+
+  before do
+    User.create!(name: "test", email: "test@test.com", password: "testtest")
+  end
+  scenario "一覧" do
+    visit admin_users_path
+
+    expect(page).to have_content "ユーザーリスト"
+  end
+
+  scenario "名前更新" do
+    visit admin_users_path
+    click_on "編集"
+    fill_in "名前", with:"testtest"
+    fill_in "メールアドレス", with:"test@test.com"
+    fill_in "パスワード", with:"testtest"
+    fill_in "もう一度入力してください", with: "testtest"
+    click_on "更新する"
+
+    expect(page).to have_content "ユーザーリスト"
+  end
+
+  scenario "閲覧" do
+    visit admin_users_path
+    click_on "詳細"
+
+    expect(page).to have_content "マイページ"
+  end
+
+  scenario "名前更新" do
+    visit admin_users_path
+    click_on "削除"
+
+    expect(page).to have_content "削除しました"
+  end
+
+end
