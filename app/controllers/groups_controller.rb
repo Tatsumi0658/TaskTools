a7e52f5e5d11debc302a106ab1aad8d2d062061d
koda <hogehogehoge@gmail.com>
2019-04-30 11:22:20 +0900

    Group機能追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,6 @@ class GroupsController < ApplicationController
 
   def index
     @groups = Group.all
-    @usergroups = Usergroup.all
   end
 
   def new
@@ -13,15 +12,21 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to todotasks_path
-      flash[:success]= "成功しました"
+      Usergroup.create(user_id: current_user.id, group_id: @group.id)
+      redirect_to groups_path
+      flash[:success] = "成功しました"
     else
       render :new
     end
   end
 
   def show
-    @usergroup = current_user.usergroups.find_by(group_id: @group.id)
+    if current_user.usergroups.find_by(group_id: @group.id)
+      @usergroup = current_user.usergroups.find_by(group_id: @group.id)
+    else
+      redirect_to groups_path
+      flash[:danger] = "権限がありません"
+    end
   end
 
   def edit
@@ -29,7 +34,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to todotasks_path
+      redirect_to groups_path
       flash[:success]= "更新しました"
     else
       render :edit
@@ -38,7 +43,7 @@ class GroupsController < ApplicationController
 
   def destroy
     @group.destroy
-    redirect_to todotasks_path
+    redirect_to groups_path
     flash[:danger]= "削除しました"
   end
 
--- app/controllers/usergroups_controller.rb
+++ app/controllers/usergroups_controller.rb
@@ -1,11 +1,26 @@
 class UsergroupsController < ApplicationController
   def create
-    usergroup = current_user.usergroups.create(group_id: params[:group_id])
-    redirect_to groups_path
+    if Usergroup.where(user_id: current_user.id).where(group_id: params[:group_id]).present?
+      redirect_to groups_path
+      flash[:danger] = "登録済みです"
+    else
+      usergroup = current_user.usergroups.create(group_id: params[:group_id])
+      redirect_to groups_path
+    end
   end
 
   def destroy
-    usergroup = current_user.usergroups.find_by(id: params[:id]).destroy
-    redirect_to groups_path
+    if @target = Group.find_by(id: params[:group_id])
+      if @target.owner_id != current_user.id
+        usergroup = current_user.usergroups.find_by(id: params[:id]).destroy
+        redirect_to groups_path
+      else
+        redirect_to groups_path
+        flash[:danger] = "グループオーナーは退出できません"
+      end
+    else
+      usergroup = current_user.usergroups.find_by(id: params[:id]).destroy
+      redirect_to groups_path
+    end
   end
 end
